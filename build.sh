842c97411d4d2ac38c050a15b5dafb1977851fd6
Mohammadreza Jafari <mohammadrezajafari.dev@gmail.com>
2025-08-20 12:41:29 +0330

    feat: add build makefile and script

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "Building with vendored dependencies..."
+
+# Vendor dependencies first
+go mod vendor
+
+PLATFORM="$1"
+PROCESSOR="$2"
+ARCHITECT="$3"
+
+if [ -z "$PLATFORM" ]; then
+    echo "please define platform (linux/darwin/windows)"
+    exit 1
+fi
+if [ -z "$PROCESSOR" ]; then
+    echo "please define processor (amd/arm)"
+    exit 1
+fi
+if [ -z "$ARCHITECT" ]; then
+    echo "please define architect (32/64)"
+    exit 1
+fi
+
+# Compose GOARCH properly
+GOARCH=""
+if [ "$PROCESSOR" = "amd" ] && [ "$ARCHITECT" = "64" ]; then
+    GOARCH="amd64"
+elif [ "$PROCESSOR" = "amd" ] && [ "$ARCHITECT" = "32" ]; then
+    GOARCH="386"
+elif [ "$PROCESSOR" = "arm" ] && [ "$ARCHITECT" = "64" ]; then
+    GOARCH="arm64"
+elif [ "$PROCESSOR" = "arm" ] && [ "$ARCHITECT" = "32" ]; then
+    GOARCH="arm"
+else
+    echo "Unsupported processor/architecture combination: $PROCESSOR/$ARCHITECT"
+    exit 1
+fi
+
+# Build static binary
+CGO_ENABLED=0 \
+GOOS="$PLATFORM" \
+GOARCH="$GOARCH" \
+go build -mod=vendor \
+    -a \
+    -ldflags="-s -w -extldflags '-static'" \
+    -trimpath \
+    -installsuffix cgo \
+    -o mongoes 
+# Clean up
+rm -rf vendor/
+
+echo "Built: $(du -h mongoes | cut -f1)"
+ldd mongoes 2>&1 || file mongoes
---
+++ makefile
@@ -0,0 +1,4 @@
+build_linux_amd_64:
+	./build.sh linux amd 64
+run:
+	@air -c .air.toml
